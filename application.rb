3ca9336225f1c8dacd50eefcd03fa9f0d4b9ec7f
Benjamin Lehn <ben.lehn@gmail.com>
2012-10-01 17:28:30 -0400

    added tracks page

--- application.rb
+++ application.rb
@@ -3,13 +3,22 @@ require 'nokogiri'
 require 'open-uri'
 require 'haml'
 
-
 get '/' do
-  albumsPath = File.open('pitchfork_albums.html')
-  albumsData = Nokogiri::HTML(albumsPath.read, nil, 'utf-8')
-  @albums = albumsData.css('li')
   haml :index
 end
 
+get '/albums' do
+  albumsPath = File.open('pitchfork_albums.html')
+  albumsData = Nokogiri::HTML(albumsPath.read, nil, 'utf-8')
+  @albums = albumsData.css('li')
+  haml :albums
+end
+
+get '/tracks' do
+  tracksPath = File.open('pitchfork_tracks.html')
+  tracksData = Nokogiri::HTML(tracksPath.read, nil, 'utf-8')
+  @tracks = tracksData.css('li')
+  haml :tracks
+end
 
 
--- rakefile.rb
+++ rakefile.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 
 # Scrape Pitchfork and write to a file
 task :scrape do
+
   File.open('pitchfork_albums.html','w') do |f|
     1.upto(3) do |page|
       pitchforkAlbumsURL = 'http://pitchfork.com/best/high-scoring-albums/' + page.to_s
@@ -10,6 +11,15 @@ task :scrape do
       f.puts pitchforkAlbumsData.css('.object-grid ul li')
     end
   end
+
+  File.open('pitchfork_tracks.html','w') do |f|
+    1.upto(6) do |page|
+      pitchforkTracksURL = 'http://pitchfork.com/reviews/best/tracks/' + page.to_s
+      pitchforkTracksData = Nokogiri::HTML(open(pitchforkTracksURL).read, nil, 'utf-8')
+      f.puts pitchforkTracksData.css('.object-list li')
+    end
+  end
+
 end
 
 
